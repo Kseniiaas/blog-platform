c21609ab12f924719e08c734dd7b33ab3100d97b
Kseniia Ivanova <xseniia2017@yandex.ru>
2025-03-29 20:13:18 +0300

    fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,39 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link, useNavigate } from 'react-router-dom';
 import './Header.scss';
 
-const Header = ({ isLoggedIn, user, setIsLoggedIn, setUser }) => {
+const Header = ({ isLoggedIn, setIsLoggedIn, setUser }) => {
   const navigate = useNavigate();
+
+  const [localUser, setLocalUser] = useState(null);
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    const storedUsername = localStorage.getItem('username');
+    const storedImage = localStorage.getItem('userImage');
+
+    if (storedToken && storedUsername) {
+      setLocalUser({ username: storedUsername, image: storedImage });  
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, [setIsLoggedIn]); 
 
   const handleLogout = () => {
     localStorage.removeItem('token');
     localStorage.removeItem('username');
     localStorage.removeItem('userImage');
-    setIsLoggedIn(false);
+    setIsLoggedIn(false); 
     setUser(null);
     navigate('/');
   };
 
-
   const defaultImage = "https://static.productionready.io/images/smiley-cyrus.jpg";
-
   return (
     <header className="header">
       <div className="header-left">
-        <Link to="/" className='header-t'>Realworld Blog</Link>
+        <Link to="/" className="header-t">Realworld Blog</Link>
       </div>
       <div className="header-right">
         {isLoggedIn ? (
           <>
             <Link to="/new-article" className="create-article-btn">Create Article</Link>
-            {user && (
+            {localUser && ( 
               <Link to="/profile" className="user-info">
-                <span className='username'>{user.username}</span>
-                <img 
-                  src={user.image?.trim() || defaultImage}
-                  alt={user.username} 
-                  className="avatar" 
+                <span className="username">{localUser.username}</span>
+                <img
+                  src={localUser.image?.trim() || defaultImage}  
+                  alt={localUser.username}
+                  className="avatar"
+                  onError={(e) => e.target.src = defaultImage}
                 />
               </Link>
             )}
@@ -52,15 +65,14 @@ const Header = ({ isLoggedIn, user, setIsLoggedIn, setUser }) => {
 
 Header.propTypes = {
   isLoggedIn: PropTypes.bool.isRequired,
-  user: PropTypes.shape({
-    username: PropTypes.string,
-    image: PropTypes.string,
-  }),
   setIsLoggedIn: PropTypes.func.isRequired,
   setUser: PropTypes.func.isRequired,
 };
 
 export default Header;
+
+
+
 
 
 
